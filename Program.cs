0f5fa045c2675f896eb18b1dc1cb53b089136106
BraswellMcMeans <98614497+BraswellMcMeans@users.noreply.github.com>
2022-04-24 14:03:54 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FileHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentFile myDataHandler = new StudentFile("students.txt");
+
+            Student[] myStudents = myDataHandler.GetAllStudents();
+
+            StudentReports reports = new StudentReports(myStudents);
+
+            reports.PrintAllStudents();
+
+            Utility myUtility = new Utility(myStudents);
+
+            myUtility.Sort();
+
+            System.Console.WriteLine("After sort -------------------------");
+
+            reports.PrintAllStudents();
+
+            string searchVal = "Makayla Townsend";
+
+            int found = myUtility.Search(searchVal);
+            if(found != -1)
+            {
+                System.Console.WriteLine("What should the credit hours be?");
+                int updatedHours = int.Parse(Console.ReadLine());
+                myStudents[found].SetCreditHours(updatedHours);
+            }
+
+            myDataHandler.Save(myStudents);
+        }
+    }
+}
---
+++ StudentReports.cs
@@ -0,0 +1,21 @@
+using System;
+namespace FileHandling
+{
+    public class StudentReports
+    {
+        private Student[] myStudents;
+
+        public StudentReports(Student[] myStudents)
+        {
+            this.myStudents = myStudents;
+        }
+
+        public void PrintAllStudents()
+        {
+            for(int i = 0; i < Student.GetCount(); i++)
+            {
+                Console.WriteLine(myStudents[i].ToString());
+            }
+        }
+    }
+}
